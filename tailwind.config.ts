c2fe00a6bb9ab6e8d8d873b70d94adcd392d287a
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-10-09 12:46:51 +0330

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './sections/**/*.{html,js,jsx,ts,tsx}',
+    './styles/**/*.{js,jsx,ts,tsx}',
+  ],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        'primary-black': '#1A232E',
+        'secondary-white': '#c7c7c7',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      transitionTimingFunction: {
+        'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
