91eae2a59e4802031eb8a6e826709be91da33b4a
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-10-10 14:54:03 +0330

    fix & deploy

--- app/components/Insights.tsx
+++ app/components/Insights.tsx
@@ -5,7 +5,8 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { insights } from '../../constants/index';
 import { staggerContainer } from '../../utils/motion';
-import { InsightCard, TitleText, TypingText } from '.';
+import {TitleText, TypingText } from './CustomTexts';
+import InsightCard from './InsightCard';
 
 const Insights = () => (
   <section className={`${styles.paddings} relative z-10`}>
