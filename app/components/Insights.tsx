536a13b9558652a160cada99221149ca1c0dee91
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-10-10 14:16:39 +0330

    completed world and insights

--- app/components/Insights.tsx
+++ app/components/Insights.tsx
@@ -3,14 +3,14 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { insights } from '../constants';
-import { staggerContainer } from '../utils/motion';
-import { InsightCard, TitleText, TypingText } from '../components';
+import { insights } from '../../constants/index';
+import { staggerContainer } from '../../utils/motion';
+import { InsightCard, TitleText, TypingText } from '.';
 
 const Insights = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <motion.div
-      variants={staggerContainer}
+      variants={staggerContainer({})}
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
